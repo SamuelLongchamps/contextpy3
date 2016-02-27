e042e2f54952ae47a6ffb5cf2013046b9e058de4
memo <memo@localhost.localdomain>
2016-02-27 17:28:37 -0500

    Initial commit, including port with unchanged behavior compared to python 2 v1.1 of the ContextPy library

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# contextpy3
+# ContextPy3
 Python3 port of the ContextPy library implementing Context-Oriented Programming as Layer-In-Class
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="contextpy3",
+      version="2.0",
+      description="Context-oriented Programming for Python 3",
+      author="Original=[Christian Schubert, Michael Perscheid], PortAndImprovements=[Samuel Longchamps]",
+      author_email="michael.perscheid@hpi.uni-potsdam.de, samuel.longchamps@usherbrooke.ca",
+      py_modules=['contextpy3'])
